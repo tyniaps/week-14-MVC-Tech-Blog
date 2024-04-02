1b2fb529219da89a324116e1f7afbedc90c32029
KnightNimbus <ty.s.spain@gmail.com>
2024-04-02 22:22:23 +0100

    basic setup for servier.js file

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars' );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, public)));
+
+app.use(routes);
+
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
